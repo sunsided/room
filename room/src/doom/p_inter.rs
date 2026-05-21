0bb745937d65a0417a0b1e0161587c39a23cc7c0
Markus Mayer <widemeadows@gmail.com>
2026-05-21 20:53:39 +0200

    Enhance comments in p_inter.rs for better understanding
    Added comments for clarity in p_inter.rs

--- room/src/doom/p_inter.rs
+++ room/src/doom/p_inter.rs
@@ -373,10 +373,14 @@ pub unsafe extern "C" fn P_TouchSpecialThing(special: *mut mobj_t, toucher: *mut
 
     let mut sound: c_int = Sfx::Itemup as c_int;
     let player = (*toucher).player as *mut PlayerT;
+    
+    // Dead thing touching.
+    // Can happen with a sliding player corpse.
     if (*toucher).health <= 0 {
         return;
     }
 
+    // Identify by sprite.
     match (*special).sprite {
         // armor
         SPR_ARM1 => {
